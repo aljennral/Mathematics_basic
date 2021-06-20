1270eeb0853457e7821e6f2cfabb5612d3d25455
Mohamed Almojtba <39987693+aljennral@users.noreply.github.com>
2021-06-20 12:27:51 +0200

    Update rootsapp.m

--- rootsapp.m
+++ rootsapp.m
@@ -4,8 +4,8 @@ function [x1,x2]=rootsapp(a,b,c)
 % SoftMath Company in www.softmathnet.com
 % x1 x2 = rootsapp(a,b,c) returns the tow roots of equation normal or imagnry.
 d=b^2-4*a*c;
-x1=(-b+sqrt(d))/(2*a);
-x2=(-b-sqrt(d))/(2*a);
+x1=(-b+sqrt(d))/(2*a)
+x2=(-b-sqrt(d))/(2*a)
 if d>0
     disp('roots is normal')
 else disp('roots is imagnry')
